8349689dd05df2d50dc11eb64b43bdad034b9c9c
Sai Kiran <saikiran.madupu@gmail.com>
2021-10-29 22:47:07 -0700

    updating files

--- labs/lab3/app.py
+++ labs/lab3/app.py
@@ -11,17 +11,18 @@ def playground():
     return PLAYGROUND_HTML, 200
 
 
-@app.route("/", methods=["POST"])
+@app.route('/graphql', methods=['POST'])
 def graphql_server():
     data = request.get_json()
     success, result = graphql_sync(
-        schema=schema, data=data, context_value=request, debug=app.debug
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
     )
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
 
 if __name__ == "__main__":
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.run(debug=True)
--- labs/lab3/resolvers.py
+++ labs/lab3/resolvers.py
@@ -5,28 +5,52 @@ query = QueryType()
 mutation = MutationType()
 
 
+# {
+#   students(student_id:1238125) {
+#     student_id
+#     name
+#   }
+# }
+@query.field("students")
+def resolve_get_student(_, info, student_id):
+    student = database.get_student(student_id=student_id)
+    return student
+
+
+# {
+#   classes(class_id:1238125) {
+#     class_id
+#     name
+#   }
+# }
+@query.field("classes")
+def resolve_get_class(_, info, class_id):
+    classs = database.get_class(class_id=class_id)
+    return classs
+
+
+
+
 @mutation.field("create_student")
 def resolve_create_student(_, info, name):
     student = database.create_student(name=name)
     return student
 
 
+# mutation create_class($name: String!) {
+#   create_class(name: $name){
+#     class_id
+#   	name
+#     students
+#   }
+# }
 @mutation.field("create_class")
 def resolve_create_class(_, info, name):
     classs = database.create_class(name=name)
     return classs
 
 
-@query.field("students")
-def resolve_student(_, info, id):
-    student = database.get_student(id)
-    return student
-
-
-@query.field("classes")
-def resolve_class(_, info, id):
-    classs = database.get_class(id)
+@mutation.field("add_student_to_class")
+def resolve_add_student_to_class(_, info, class_id, student_id):
+    classs = database.add_student_to_class(class_id=class_id, student_id=student_id)
     return classs
-
-
-
